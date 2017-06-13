bc39677c9b0c083ff4be92b828bac83cf4404e6b
Feiyu Lu <billthefreeman@gmail.com>
2017-06-13 10:33:49 -0400

    First commit

---
+++ client/reducers/posts.js
@@ -0,0 +1,21 @@
+//a recuder takes in 2 things
+
+//1. the action
+//2. copy of current state
+
+function posts(state=[], action){
+	switch(action.type){
+		case 'INCREMENT_LIKES' :
+			const i = action.index;
+			return [
+				...state.slice(0,i),
+				{...state[i],likes: state[i].likes+10},
+				...state.slice(i+1),
+			]
+		default:
+			return state;
+	}
+	
+	return state;
+}
+export default posts;
---
+++ client/store.js
@@ -0,0 +1,29 @@
+import {createStore,compose} from 'redux';
+import {syncHistoryWithStore} from 'react-router-redux';
+import {browserHistory} from 'react-router';
+
+//root reducer
+import rootReducer from './reducers/index';
+
+import comments from './data/comments';
+import posts from './data/posts';
+
+//create an object for the default data
+const defaultState = {
+	posts,
+	comments
+};
+
+const store = createStore(rootReducer,defaultState);
+
+export const history = syncHistoryWithStore(browserHistory,store);
+
+// hot loading
+if (module.hot) {
+	module.hot.accept('./reducers/',()=>{
+		const nextRootReducer = require('./reducers/index').default;
+		store.replaceReducer(nextRootReducer);
+	});
+}
+
+export default store;
